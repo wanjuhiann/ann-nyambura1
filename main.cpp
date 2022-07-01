4ae870451bcd54c7aa8a42727c5ca000ab71c90b
wanjuhiann <106369744+wanjuhiann@users.noreply.github.com>
2022-07-01 11:23:07 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int product;
+    cout << "Enter a" << endl;
+    cin>>a;
+    cout<< "Enter b" <<endl;
+    cin>>b;
+    product=a*b;
+    cout<<"The product of the numbers is:" <<product;
+
+
+    return 0;
+}
